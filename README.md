051598cb2b2925bcc424bd9e2a3de9020ab3d17a
naily26 <nailymasruri@gmail.com>
2022-09-06 17:22:48 +0700

    init project contacs app

---
+++ README.md
@@ -0,0 +1,18 @@
+# contacts_app
+
+
+## Output
+
+1. Create 
+![screenshoot contacts_app](images/create.png)
+
+2. Read
+![screenshoot contacts_app](images/output_1.png)
+
+3. Update
+![screenshoot contacts_app](images/update.png)
+
+4. Delete
+![screenshoot contacts_app](images/delete.png)
+
+
---
+++ lib/form_kontak.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'database/db_helper.dart';
+import 'model/kontak.dart';
+
+class FormKontak extends StatefulWidget {
+  final Kontak? kontak;
+  FormKontak({this.kontak});
+
+  @override
+  State<FormKontak> createState() => _FormKontakState();
+}
+
+class _FormKontakState extends State<FormKontak> {
+  DbHelper db = DbHelper();
+  TextEditingController? name; 
+  TextEditingController? lastName; 
+  TextEditingController? mobileNo; 
+  TextEditingController? email; 
+  TextEditingController? company;
+
+  @override
+  void initState() { 
+    name = TextEditingController( 
+      text: widget.kontak == null ? '' :widget.kontak!.name);
+    mobileNo = TextEditingController( 
+      text: widget.kontak == null ? '' :widget.kontak!.mobileNo);
+    email = TextEditingController( 
+      text: widget.kontak == null ? '' :widget.kontak!.email);
+    company= TextEditingController( 
+      text: widget.kontak == null ? '' :widget.kontak!.company);
+  super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Form Kontak'), 
+      ),
+       body: ListView(
+        padding: EdgeInsets.all(16.0), 
+        children: [ 
+          Padding(
+            padding: const EdgeInsets.only(top: 20, ), 
+            child: TextField(
+              controller: name,
+              decoration: InputDecoration(
+                labelText: 'Name',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  )
+              ),
+            ),
+          ),
+           Padding(
+            padding: const EdgeInsets.only(top: 20, ), 
+            child: TextField(
+              controller: mobileNo,
+              decoration: InputDecoration(
+                labelText: 'Mobile No',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  )
+              ),
+            ),
+          ),
+           Padding(
+            padding: const EdgeInsets.only(top: 20, ), 
+            child: TextField(
+              controller: email,
+              decoration: InputDecoration(
+                labelText: 'email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  )
+              ),
+            ),
+          ),
+           Padding(
+            padding: const EdgeInsets.only(top: 20, ), 
+            child: TextField(
+              controller: company,
+              decoration: InputDecoration(
+                labelText: 'company',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                  )
+              ),
+            ),
+          ),
+           Padding(
+            padding: const EdgeInsets.only(top: 20, ), 
+            child: ElevatedButton(
+              child: (widget.kontak == null)
+                ? Text('add',
+                style: TextStyle(color: Colors.white),
+                )
+                : Text('upadate',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                upsertKontak();
+              },
+            )
+          ),
+        ]
+       ),
+    );
+  }
+
+  Future<void> upsertKontak() async {
+    if(widget.kontak!= null) {
+      await db.updateKontak(Kontak(
+        id: widget.kontak!.id,
+        name: name!.text,
+        mobileNo: mobileNo!.text,
+        email: email!.text,
+        company: company!.text
+      ));
+      Navigator.pop(context, 'update');
+    } else {
+      await db.saveKontak(Kontak(
+        name: name!.text,
+        mobileNo: mobileNo!.text,
+        email: email!.text,
+        company: company!.text
+      ));
+      Navigator.pop(context, 'save');
+    }
+  }
+}
